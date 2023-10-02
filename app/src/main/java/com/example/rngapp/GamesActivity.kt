2457c8e9feda8be7546ba244741358353cfb99fa
DESKTOP-R1JE80S\Administrator <unoshiki12@gmail.com>
2023-10-02 23:00:32 +0800

    add animation

--- app/src/main/java/com/example/rngapp/GamesActivity.kt
+++ app/src/main/java/com/example/rngapp/GamesActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -34,6 +35,7 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.graphics.Color
@@ -97,7 +99,7 @@ class GamesActivity : ComponentActivity() {
                             contentDescription = "Image",
                             modifier = Modifier
                                 .fillMaxWidth()
-                                .height(200.dp).clickable { connect() }
+                                .height(200.dp).clip(shape = RoundedCornerShape(20.dp)).clickable { connect() }
                         )
                         Spacer(modifier = Modifier.height(30.dp))
                     }
--- app/src/main/java/com/example/rngapp/RandomActivity.kt
+++ app/src/main/java/com/example/rngapp/RandomActivity.kt
@@ -3,8 +3,9 @@ package com.example.rngapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,21 +19,23 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.drawText
@@ -42,7 +45,73 @@ import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
 import androidx.compose.ui.text.rememberTextMeasurer
 import kotlinx.coroutines.delay
+import kotlin.math.sqrt
 
+@Composable
+fun CircleView(interval: Float){
+    Canvas(modifier = Modifier.fillMaxSize().rotate(interval*200f)){
+        drawArc(
+            color = Color.Red,
+            startAngle = 10f,
+            sweepAngle = 100f,
+            useCenter = false,
+            topLeft = Offset(0f, 0f),
+            size = Size(size.width, size.height),
+            style = Stroke(width = 3f)
+        )
+        drawArc(
+            color = Color.Red,
+            startAngle = 130f,
+            sweepAngle = 100f,
+            useCenter = false,
+            topLeft = Offset(0f, 0f),
+            size = Size(size.width, size.height),
+            style = Stroke(width = 3f)
+        )
+        drawArc(
+            color = Color.Red,
+            startAngle = 250f,
+            sweepAngle = 100f,
+            useCenter = false,
+            topLeft = Offset(0f, 0f),
+            size = Size(size.width, size.height),
+            style = Stroke(width = 3f)
+        )
+        drawCircle(color = Color.Red, radius = 6f, center = Offset(size.width, size.height/2),style = Stroke(width = 3f))
+        drawLine(color = Color.Red, start = Offset(size.width-2* sqrt(2f), size.height/2 - 2* sqrt(2f)), end=Offset(size.width+2* sqrt(2f), size.height/2 + 2* sqrt(2f)), strokeWidth = 3f)
+        drawLine(color = Color.Red, start = Offset(size.width-2* sqrt(2f), size.height/2 + 2* sqrt(2f)), end=Offset(size.width+2* sqrt(2f), size.height/2 - 2* sqrt(2f)), strokeWidth = 3f)
+        drawCircle(color = Color.Red, radius = 6f, center = Offset(size.width/4, size.height/2 * (1 + sqrt(3f)/2)),style = Stroke(width = 3f))
+        drawCircle(color = Color.Red, radius = 6f, center = Offset(size.width/4, size.height/2 * (1 - sqrt(3f)/2)),style = Stroke(width = 3f))
+    }
+}
+@Composable
+fun MultiplierView(interval: Float){
+    Canvas(modifier = Modifier.fillMaxSize().rotate(-interval*200f)){
+        var radius = size.width * 0.375f
+        drawArc(
+            color = Color.Red,
+            startAngle = 10f,
+            sweepAngle = 160f,
+            useCenter = false,
+            topLeft = Offset(size.width/2f - radius, size.height/2f - radius),
+            size = Size(radius * 2f, radius* 2f),
+            style = Stroke(width = 3f)
+        )
+        drawArc(
+            color = Color.Red,
+            startAngle = 190f,
+            sweepAngle = 160f,
+            useCenter = false,
+            topLeft = Offset(size.width/2f - radius, size.height/2f - radius),
+            size = Size(radius* 2f, radius* 2f),
+            style = Stroke(width = 3f)
+        )
+        drawCircle(color = Color.Red, radius = 6f, center = Offset(size.width / 2 + radius, size.height/2),style = Stroke(width = 3f))
+        drawLine(color = Color.Red, start = Offset(size.width / 2 + radius-2* sqrt(2f), size.height/2 - 2* sqrt(2f)), end=Offset(size.width / 2 + radius+2* sqrt(2f), size.height/2 + 2* sqrt(2f)), strokeWidth = 3f)
+        drawLine(color = Color.Red, start = Offset(size.width / 2 + radius-2* sqrt(2f), size.height/2 + 2* sqrt(2f)), end=Offset(size.width / 2 + radius+2* sqrt(2f), size.height/2 - 2* sqrt(2f)), strokeWidth = 3f)
+        drawCircle(color = Color.Red, radius = 6f, center = Offset(size.width/2 - radius, size.height/2),style = Stroke(width = 3f))
+    }
+}
 class RandomActivity : ComponentActivity() {
     @OptIn(ExperimentalTextApi::class, ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -69,21 +138,30 @@ class RandomActivity : ComponentActivity() {
                     val textMeasurer = rememberTextMeasurer(cacheSize = 0)
 
                     val coroutineScope = rememberCoroutineScope()
+                    val animationScope = rememberCoroutineScope()
                     val textValue = remember { mutableStateOf(0) }
+                    val interval = remember { mutableStateOf(0f) }
                     LaunchedEffect(UInt){
+                        interval.value = 0f
                         coroutineScope.launch {
                             while (true) {
-                                delay(2000)
-                                textValue.value = (0..10000).random()
+                                delay(5000)
+                                textValue.value = (110..2900).random()
 
                             }
                         }
+                        animationScope.launch {
+                            while(true){
+                                delay(20)
+                                interval.value = interval.value + 0.001f
+                            }
+                        }
                     }
-                    Image(painterResource(R.drawable.display), contentDescription = "Logo", modifier = Modifier
-                        .width(200.dp)
-                        .height(200.dp)
-                        .drawWithContent {
-                            drawContent()
+                    Box(modifier = Modifier.height(200.dp).width(200.dp)){
+                        CircleView(interval.value)
+                        MultiplierView(interval.value)
+                        Canvas(modifier = Modifier
+                            .fillMaxSize()){
                             val formattedText =
                                 String.format("%.2fX", ((textValue.value).toFloat() / 100.0))
                             val textLayoutResult = textMeasurer.measure(
@@ -103,8 +181,38 @@ class RandomActivity : ComponentActivity() {
                                     (size.height - textLayoutResult.size.height) / 2,
                                 )
                             )
-                        })
-                    Spacer(modifier = Modifier.height(20.dp))
+                        }
+                    }
+
+
+//                    Image(painterResource(R.drawable.display), contentDescription = "Logo", modifier = Modifier
+//                        .width(200.dp)
+//                        .height(200.dp)
+//                        .drawWithContent {
+//                            drawContent()
+//                            val formattedText =
+//                                String.format("%.2fX", ((textValue.value).toFloat() / 100.0))
+//                            val textLayoutResult = textMeasurer.measure(
+//                                text = formattedText,
+//                                style = TextStyle(
+//                                    fontSize = 32.sp,
+//                                    fontWeight = FontWeight.Bold,
+//                                ),
+//                                overflow = TextOverflow.Ellipsis
+//
+//                            )
+//                            drawText(
+//                                textLayoutResult,
+//                                color = Color.Red,
+//                                topLeft = Offset(
+//                                    (size.width - textLayoutResult.size.width) / 2,
+//                                    (size.height - textLayoutResult.size.height) / 2,
+//                                )
+//                            )
+//                        })
+
+
+                    Spacer(modifier = Modifier.height(50.dp))
                     Button(onClick = { }, colors = ButtonDefaults.buttonColors(containerColor = Color.Green, contentColor = Color.White)) {
                         Text("CONNECTED", color=Color.Gray)
                     }
@@ -116,5 +224,5 @@ class RandomActivity : ComponentActivity() {
             }
         }
     }
-
 }
+
